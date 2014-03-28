76d7b72b88b40fd6306fc376ca09dcfd075caf2b
Thomas C Green <TomG@AccelHomeBuyers.com>
2014-03-28 17:07:52 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecarei::Application.config.secret_key_base = 'e328e4e59a7a374dd140275ea53e0483f7388146db7e55adacdfe12967a6177f18246c9e5e8228b6aaf6c64b45f2a22935152ff8fb92811f4ada5f900938f0c1'
