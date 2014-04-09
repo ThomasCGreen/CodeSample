7cb81f665eef975b8a80b24b3f46028de1baf9fc
Thomas C Green <TomG@AccelHomeBuyers.com>
2014-04-09 18:16:38 +0000

    * Found needed test for adding multiple categeories so added it here.
    [#68331026]

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -1,16 +1,14 @@
 module DisplayHelper
   def category_helper(category)
+    link_id = category.title + '_' + category.id.to_s
     content_tag(:li,
                 category.title.html_safe + " ".html_safe +
                     link_to(t('.addcategory',
-                              :default => t("helpers.links.addcategory")),
+                              default: t("helpers.links.addcategory")),
                             new_category_category_path(category.id).html_safe,
-                            :class => 'btn btn-mini').html_safe +
-                                subcategories(category.categories)
-                    # link_to('Add Category',
-                    #         new_category_category_path(
-                    #             category_id: category.id)).html_safe +
-                    #             subcategories(category.categories)
+                            class: 'btn btn-mini').html_safe +
+                                subcategories(category.categories),
+                id: link_id
     )
   end
 
--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -27,4 +27,41 @@ feature 'Recommendations Categories' do
     expect(page).to have_text('The following are resources you can explore')
     expect(page).to have_text('Lenders')
   end
+
+  scenario 'allows user to create the second category' do
+    @user = FactoryGirl.create(:user)
+    @category = Category.new
+    @category.title = "Resources"
+    @category.save
+
+    @category1 = Category.new
+    @category1.title = "Flooring"
+    @category1.category_id = @category.id
+    @category1.save
+
+    @category2 = FactoryGirl.create(:category)
+    @category2.category_id = @category1.id
+    @category2.save
+
+    visit '/'
+    click_link 'Log In'
+
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+
+    click_button 'Sign In'
+
+    within("//li[@id='#{@category1.title}_#{@category1.id}']") do
+      first(:link, "Add Category").click
+    end
+
+    fill_in 'Title', with: 'Lenders'
+
+    click_button 'Create New Category'
+
+    expect(page).to have_text('The following are resources you can explore')
+    expect(page).to have_text('Lenders')
+    expect(page).to have_text(@category1.title)
+    expect(page).to have_text(@category2.title)
+  end
 end
