cb1da2892baafb28325928280469ad84daa1f8be
Thomas C Green <TomG@AccelHomeBuyers.com>
2014-04-12 22:14:18 +0000

    symlink database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,11 +59,11 @@ namespace :deploy do
 
 end
 
-after 'deploy:update_code', 'deploy:symlink_db'
-
 namespace :deploy do
   desc "Symlinks the database.yml"
   task :symlink_db, :roles => :app do
     run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
   end
 end
+
+after 'deploy:update_code', 'deploy:symlink_db'
